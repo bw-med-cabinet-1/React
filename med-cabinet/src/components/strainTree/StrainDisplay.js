2dcac2849b6cb55ebd16f96a1011a75b1f95cf72
tarmes <traarmes@gmail.com>
2020-09-24 16:47:10 -0400

    strainList displaying

--- med-cabinet/src/components/strainTree/StrainDisplay.js
+++ med-cabinet/src/components/strainTree/StrainDisplay.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchApi } from "../../api/fetchApi";
+import styled from 'styled-components'
 import { Card, Button, CardTitle, CardText, Row, Col, Container } from 'reactstrap'
 
+const Styles = styled.div`
+    .strainList-container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+    
+    .card-container {
+        width: 45%;
+    }
+`
+    
+
 export default function StrainDisplay() {
+
+    const [strainList, setStrainList] = useState([]);
+    const getStrains = () => {
+        fetchApi()
+        .then(res => {
+            console.log(res.data, "this is the data");
+            setStrainList(res.data);
+         })
+        .catch(err => {
+        console.log(err, "there's an error");
+        });
+    };
+    useEffect(() => {
+        getStrains()
+    }, []);
+    
     return (
-        <Row>
-            <Col sm="6">
-              <Card body>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+        <Styles>
+        <Container><h3> Strain List</h3>
+        <Container className="strainList-container">
+        {strainList.map((strain) => (
+            <Container className='card-container'>
+            <h4 key={strain.strain}>
+            <Card body className='strain-card'>
+                <CardTitle>{strain.strain}</CardTitle>
+                <CardText>{strain.description}</CardText>
                 <Button>Go somewhere</Button>
-              </Card>
-            </Col>
-            <Col sm="6">
-              <Card body>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </Card>
-            </Col>
-          </Row>
+            </Card>
+          </h4>
+            </Container>
+        ))}
+        </Container>
+        </Container>
+        </Styles>
     )
 }
