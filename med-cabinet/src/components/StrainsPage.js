cadf5a8ee427440d1a84ec9a277fbdbb7d0234f8
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-09-24 16:32:06 -0700

    fix err in StrainPage

--- med-cabinet/src/components/StrainsPage.js
+++ med-cabinet/src/components/StrainsPage.js
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from "react";
-// import StrainsCard from "./StrainsCard";
 import StrainsList from "./StrainsList";
 import { fetchApi } from "../api/fetchApi";
-// import styled from "styled-components";
+import styled from "styled-components";
 
-// const StrainsStyle = styled.div`
-//   * {
-//     font-family: "Source Sans Pro", sans-serif;
-//   }
-//   background: linear-gradient(#2d6a4f, #1b4332);
-//   margin: 1.5% auto;
-//   padding: 4%;
-//   width: 98%;
-//   color: white;
-//   height: 110vh;
+const StrainsStyle = styled.div`
+  * {
+    font-family: "Source Sans Pro", sans-serif;
+  }
+  background: linear-gradient(#2d6a4f, #1b4332);
+  margin: 1.5% auto;
+  padding: 4%;
+  width: 98%;
+  color: white;
+  height: 110vh;
 
-//   .strain-page {
-//     margin: 4% auto;
-//     padding: 2%;
-//     background-color: #081c15;
-//     width: 70%;
-//     border-radius: 15px;
-//     box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
-//   }
-// `;
+  .strain-page {
+    margin: 4% auto;
+    padding: 2%;
+    background-color: #081c15;
+    width: 70%;
+    border-radius: 15px;
+    box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+  }
+`;
 
 const StrainsPage = () => {
   const [strainList, setStrainList] = useState([]);
@@ -44,17 +43,15 @@ const StrainsPage = () => {
   }, [setStrainList]);
 
   return (
-    <div className="strain-page">
-    {/* <StrainsStyle> */}
+    <> 
+    <StrainsStyle>
       <div className="strain-page">
         <h1> Strains Page </h1>
-        <StrainsList strains={strainList} updateStrains={setStrainList} />
-        {/* <h3>Strain Informations</h3> */}
-        {/* <StrainsCard strains={StrainsCard} /> */}
-      </div>
-    {/* </StrainsStyle> */}
-   </div>
-  );
-};
+        <StrainsList strains={strainList} getStrains={getStrains} updateStrains={setStrainList} />
+        </div>
+      </StrainsStyle>
+    </>
+  )
+}
+export default StrainsPage
 
-export default StrainsPage;
